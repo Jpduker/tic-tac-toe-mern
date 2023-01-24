6f6ad3ff7830b04729661b8f68bfc3cf4a25d5fa
Jpduker <jayaprakashnis619@gmail.com>
2023-01-24 15:32:24 +0530

    added new user home screen

--- tic-tac-toe/src/HomeNewUser.js
+++ tic-tac-toe/src/HomeNewUser.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import "./HomeNewUser.css";
+import { Button } from "@mui/material";
 
 function HomeNewUser() {
-  return (
-    <div>HomeNewUser</div>
-  )
+	return (
+		<div>
+			<div className="your-name-home">Your Games</div>
+
+			<div className="no-games">No Games Found </div>
+
+            <Button className="start-new-game" variant="contained"  sx={{ color: "#FFFFF", backgroundColor: "#F2C94C" }}>Start A New Game !</Button>
+		</div>
+	);
 }
 
-export default HomeNewUser
+export default HomeNewUser;
